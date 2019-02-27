c1180f46527e81124e849aa063eb017bded04a81
imns1ght <jeffersonbrunoit@gmail.com>
2019-02-27 20:02:34 -0300

    Fix binary search

--- 2019.1/EDB_I/class/05.search/lower_upper-bound/src/bsearch.cpp
+++ 2019.1/EDB_I/class/05.search/lower_upper-bound/src/bsearch.cpp
@@ -8,59 +8,49 @@
 
 namespace bs {
 
-    /// Returns an iterator pointing to the first element in the range [first, last) that is not less than (i.e. greater or equal to) value, or last if no such element is found.
-    bs::value_type * lbound( bs::value_type * first, bs::value_type * last, bs::value_type value )
-    {
-        // TODO: Insert here your solution for the lower bound problem.
+/// Returns an iterator pointing to the first element in the range [first, last) that is not less than (i.e. greater or equal to) value, or last if no such element is found.
+bs::value_type * lbound( bs::value_type * first, bs::value_type * last, bs::value_type value )
+{
+	// TODO: Insert here your solution for the lower bound problem.
 
-        return first; // stub, not valid.
-    }
-
-
-    /// Returns an iterator pointing to the first element in the range [first, last) that is *greater* than value, or last if no such element is found.
-    bs::value_type * ubound( bs::value_type * first, bs::value_type * last, bs::value_type value )
-    {
-        // TODO: Insert here your solution for the lower bound problem.
-
-        return first; // stub, not valid.
-    }
-
-    /// Implements an interative binary search on an array of integers.
-    /*!
-     * \param first Pointer to the first element in the range.
-     * \param last Pointer past the last valid element in the range.
-     * \param value The target value we are looking for within the range.
-     * \return A pointer to the target value within the range; or last, in case the value is not in the range.
-     */
-    bs::value_type * bsearch( bs::value_type * first, bs::value_type * last, bs::value_type value )
-    {
-        // determine current length
-        auto count = std::distance( first, last );
-
-        // Keep searching and halving the range until we get to an empty range.
-        while ( count > 0 )
-        {
-            auto step = count/2; // Determine the offset to get the midpoint element in the current range.
-            auto m{ first }; // An aux iterator that points to the element located at the midpoint.
-            std::advance( m, step ); // Position `m` at the midpoint location.
-
-            // Equality test
-            if ( not ( value < *m ) and not ( *m < value ) )
-            {
-                return m;
-            }
-            else if ( *m < value ) // go right
-            {
-                count -= (step+1); // Remove the quantity of elements to get to the midpoint + the midpoint itself.
-                first = ++m; // Update the first to its new location, just past the "old" midpoint.
-            }
-            else // look into the left half.
-            {
-                count = step;
-            }
-        }
-
-        return  last;
-    }
+	return first; // stub, not valid.
 }
 
+
+/// Returns an iterator pointing to the first element in the range [first, last) that is *greater* than value, or last if no such element is found.
+bs::value_type * ubound( bs::value_type * first, bs::value_type * last, bs::value_type value )
+{
+	// TODO: Insert here your solution for the lower bound problem.
+
+	return first; // stub, not valid.
+}
+
+/// Implements an interative binary search on an array of integers.
+/*!
+ * \param first Pointer to the first element in the range.
+ * \param last Pointer past the last valid element in the range.
+ * \param value The target value we are looking for within the range.
+ * \return A pointer to the target value within the range; or last, in case the value is not in the range.
+ */
+bs::value_type * bsearch( bs::value_type * first, bs::value_type * last, bs::value_type value )
+{
+	auto distance = std::distance(first, last); // Current length
+
+	while (distance > 0) {
+		auto offset = distance / 2; // Determine the mid element of the range
+		auto m = first + offset; // Pointer to the element located in mid
+
+		if (value == *m) {
+			return m;
+		} else if (value > *m) { // If the element is on the right
+			distance -= offset + 1; // Determines the range for one element after mid 
+			first = ++m; // Determines the first element for one element after mid
+		} else if (value < *m) { // If the element is on the left
+			distance = offset; 
+		}
+	}
+
+	return last; // If the value is not found 
+}
+
+}

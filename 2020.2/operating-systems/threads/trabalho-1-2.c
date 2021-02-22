8b84bfa2c0a80835bcdec1ce508916e630bc38d8
imns1ght <jeffersonbrunoit@gmail.com>
2021-02-22 18:13:32 -0300

    WIP: implementa threads no trabalho 1.2

--- 2020.2/operating-systems/threads/trabalho-1-2.c
+++ 2020.2/operating-systems/threads/trabalho-1-2.c
@@ -7,30 +7,54 @@
 
 #define PI acos(-1)
 
+// Limites
+double a = 0;
+double b;
+double h;
+
+// (Número de trapézios / número de threads)
+int trapezoidsPerThread;
+// Número de trapézios da última thread
+int trapezoidsLastThread;
+
+double (*f)(double);  // Ponteiro para a função desejada pelo usuário
+
 double function1(double x) { return 5; }
 double function2(double x) { return sin(2 * x) + cos(5 * x); }
+
+void* calc(void* i) {
+    double total;
+    double* result;
+    int local_a = *(int*)i * trapezoidsPerThread;
+    int local_b = local_a + trapezoidsPerThread;
+
+    for (double i = local_a; i < local_b; i++) {
+        double x_i = (a + i) * h;
+        total += (*f)(x_i);
+    }
+
+    result = &total;
+
+    return result;
+}
 
 int main(int argc, char* argv[]) {
     int t = atoi(argv[2]);  // Número de threads
     int n = atoi(argv[3]);  // Número de trapézios
-    double (*f)(double);    // Ponteiro para a função desejada pelo usuário
-    char outputText[30];    // Texto a ser exibido ao final da execução
     pthread_t threads[t];
-    double threadsReturns[t];  // Valores retornado pelas threads
-
-    // Limites
-    double a = 0;
-    double b;
+    double* threadsValues[t];  // Valores retornado pelas threads
+    // (Número de trapézios / número de threads)
+    trapezoidsPerThread = n / t;
+    // Número de trapézios da última thread
+    trapezoidsLastThread = n - trapezoidsPerThread * t;
 
     // Verifica qual função o usuário deseja
     if (!strcmp(argv[1], "f1")) {
         b = 10;
         f = &function1;
-        strcpy(outputText, "Área total: %.2f\n");
     } else if (!strcmp(argv[1], "f2")) {
         b = 2 * PI;
         f = &function2;
-        strcpy(outputText, "Área total: %e\n");
     } else {
         // Verificação de erros básica
         printf("Por favor, passe como parâmetro \"f1\" ou \"f2\"\n");
@@ -38,17 +62,31 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    double h = (b - a) / n;
+    h = (b - a) / n;
     double totalArea = (f(a) + f(b)) / 2;
 
-    for (double i = 1; i < n; i++) {
-        double x_i = (a + i) * h;
-        totalArea += (*f)(x_i);
+    // for (double i = 1; i < n; i++) {
+    //     double x_i = (a + i) * h;
+    //     totalArea += (*f)(x_i);
+    // }
+
+    for (int i = 0; i < t; i++) {
+        int* thread_a = &i;
+        if (pthread_create(&threads[i], NULL, calc, thread_a) != 0) {
+            printf("Erro ao criar a thread[%d]", i);
+        } else {
+            printf("Thread[%d] criada com sucesso!\n", i);
+        }
     }
 
+    // Espera as threads finalizarem suas rotinas e adiciona os resultados
+    for (int i = 0; i < t; i++) {
+        pthread_join(threads[i], (void*)&threadsValues[i]);
+        totalArea += *threadsValues[i];
+    }
     totalArea = h * totalArea;
 
-    printf(outputText, totalArea);
+    printf("\nÁrea total: %e\n", totalArea);
 
     return 0;
 }
